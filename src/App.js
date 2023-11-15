5f9c92c0cdada27c0ef7cc8c5d47446ba9be1f75
victor <aderibigbevictor79@gmail.com>
2023-11-15 22:45:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React  from 'react';
+import './App.css';
+import { useEffect, useState } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Body from './Body';
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+    
+<Header />
+<Body />
+<Footer />
+
+
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
